9689916e330c148277af4b0b139f6cb281004817
Kelly Dwan <ryelle@users.noreply.github.com>
2024-06-14 17:03:58 -0400

    Tools/Styles: Move styles to faux-block to enable build step

--- source/wp-content/themes/wporg-themes-2024/functions.php
+++ source/wp-content/themes/wporg-themes-2024/functions.php
@@ -101,14 +101,21 @@ function set_up_rewrites() {
  * Enqueue scripts and styles.
  */
 function enqueue_assets() {
+	$asset_file = get_theme_file_path( 'build/style/index.asset.php' );
+	if ( ! file_exists( $asset_file ) ) {
+		return;
+	}
+
 	// The parent style is registered as `wporg-parent-2021-style`, and will be loaded unless
 	// explicitly unregistered. We can load any child-theme overrides by declaring the parent
 	// stylesheet as a dependency.
+
+	$metadata = require $asset_file;
 	wp_enqueue_style(
-		'wporg-theme-directory-2024-style',
-		get_stylesheet_uri(),
+		'wporg-theme-directory-2024',
+		get_theme_file_uri( 'build/style/style-index.css' ),
 		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
-		filemtime( __DIR__ . '/style.css' )
+		$metadata['version']
 	);
 }
 
